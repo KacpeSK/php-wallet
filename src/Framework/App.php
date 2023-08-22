5a7e6b26f8e9b6a6bd7c4afaeb58d23e202c80d7
KacpeSK <142713164+KacpeSK@users.noreply.github.com>
2023-08-22 18:05:50 +0200

    implementing Routing
    50%complete.

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,7 +5,23 @@ declare(strict_types=1);
 namespace Framework;
 
 class App {
-    public function run() {
-        echo "Application is runnig";
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
+    public function run() 
+    {
+        $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+        $method = $_SERVER["REQUEST_METHOD"];
+
+        $this->router->dispatch($path, $method);
+    }
+
+    public function get(string $path, array $controller)
+    {
+        $this->router->add('GET', $path, $controller);
     }
 }
