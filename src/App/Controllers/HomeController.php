d555e7512dc1142fa29f45dbea6b55bc2f2eeeb0
KacpeSK <142713164+KacpeSK@users.noreply.github.com>
2023-08-22 21:17:11 +0200

    Template engine pre Partials
    Finalizing template engine before splitting into partials

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+use App\Config\Path;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Path::VIEW);
+    }
+
     public function home()
     {
-        echo "You are visiting the HomePage";
+        echo $this->view->render("/index.php", [
+            "title" => "HomePage"
+        ]);
     }
 }
